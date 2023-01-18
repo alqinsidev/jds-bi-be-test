4cf7e0387b8a3da8adc061f2762fb72a4618ce4b
alqinsidev <padlanalqinsi@live.com>
2023-01-19 06:16:51 +0700

    fix(models): remove users model
    remove unused users model

--- src/models.py
+++ src/models.py
@@ -5,7 +5,7 @@ from .database import Base
 class Data_Stunting(Base):
     __tablename__ = 'data_stunting'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key = True)
     kode_provinsi = Column(Integer)
     nama_provinsi = Column(String)
     kode_kabupaten_kota = Column(Integer)
